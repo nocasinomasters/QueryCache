6b6a92c0bad696cf08656d8c5cfb7ebde61c6aef
ngtramy <tramy.kimchung@gmail.com>
2015-12-28 21:36:43 +0100

    Update qCache.py

--- qCache.py
+++ qCache.py
@@ -1,18 +1,51 @@
-#stores objects that have already been gathered from the web
+import socket
+
+# stores objects that have already been gathered from the web
+# do we set a limit size for the cache?
+PORT_MANAGER = 2000
+PORT_FETCHER = 5000
+
+
 class qCache:
-  dict myTable;
 
-  def checkCacheObject(objectName):
-    pass
+    def __init__(self):
+        pass
 
-  def setCacheObject(objectName, object):
-    pass
+    myTable = dict()
 
-  def getCacheObject(objectName):
-    pass
-   
-  def setupCache():
-    pass
+    def check_cache_object(objectName):
+        return objectName in qCache.myTable
 
-  def destroyCache():
-    pass
+    def set_cache_object(objectName, object):
+        qCache.myTable["url"] = objectName
+        qCache.myTable["attribute"] = object
+
+    def get_cache_object(objectName):
+        return qCache.myTable.get(objectName)
+
+    def setup_cache():
+        s_manager = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s_manager.bind(('', PORT_MANAGER))  # did not choose a hostname yet and port random
+        s_manager.listen(5)
+        return s_manager
+
+    def destroy_cache():
+        self.myTable.clear()
+
+    sman = setup_cache()
+    print('lala')
+    while True:
+        conn, conn_info = sman.accept()
+        request = conn.recv(1024)
+        if request.startswith('http'):
+            buff = request
+            conn.send(check_cache_object(request))
+            conn.close()
+        elif request.startswith('gc') and 'buff' in locals():
+                conn.send(get_cache_object(buff))
+        elif request.startswith('sc') and 'buff' in locals():
+            s_fetcher = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s_fetcher.connect('localhost', PORT_FETCHER)
+            page = s_fetcher.recv(8192)
+            set_cache_object(buff, page)
+            s_fetcher.close()
