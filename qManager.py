b05b8aa1273e3c21e975af1dc3e0eeb0a00b33d2
abbasse <abbasse.adamaly@gmail.com>
2016-01-06 20:54:40 +0100

    Update qManager.py

--- qManager.py
+++ qManager.py
@@ -1,46 +1,25 @@
 #runs the show
 import socket
-PORT_PROXY = 1000
-PORT_CACHE = 1500
-PORT_FETCHER = 5000
+from qUtils import qUtils
+import cPickle
+import time
+
 class qManager:
   def __init__(self):
   	pass
 
-  def cacheOrFetch(request):
-  	#ask to qCache if he has the data
-    	manager = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    	cache_adrr = ('',PORT_CACHE) # did not choose a hostname yet and port random
-    	manager.connect(cache_adrr)
-    	manager.send(request)
+  def cache(url_list): 
+	#ask to qCache if he has the data
+    	manager = qUtils.send_message(request,qUtils.CACHE_PORT)
+    	web_list = {}
+	data = qUtils.recv_message(qUtils.CACHE_PORT)
+	if data != 'END' : 
+		web_list.update(data) #add data to web_list
+	return web_list
 
-    	data = manager.recv(8192) 
-	return data
-
-  def setup_Manager():
-  	s_proxy = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s_proxy.bind(('', PORT_PROXY))  # did not choose a hostname yet and port random
-        s_proxy.listen(5)
-        return s_proxy
-
-  s = setup_Manager()
-
-  while True:
-    conn,addr = s.accept() #accept connection from proxy
-    request = conn.recv(8192) #receive proxy data (buffer_size = 8192)
-
-    data = cacheOrFetch(request)
-
-    #if yes open a socket and send to proxy
-    if data == 'yes' : 
-      #conn.sendall() #don't know what to send because i don't know what kind of data we need to give back
-
-    else: #if not open a socket and send to fetcher when we have answer we open a socket and send to proxy
-      manager = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-      fetcher_adrr = ('',PORT_FETCHER) # did not choose a hostname yet and port random
-      manager.connect(fetcher_adrr)
-      manager.send(request)
-      data = manager.recv(8192) 
-
-    #conn.sendall(senddata)
-    conn.close()
+while True:
+	url_list = qUtils.recv_message(qUtils.MGR_PORT) #receive proxy data 
+	print 'RECV %s'%cPickle.loads(url_list)
+	test = []
+	time.sleep(3)
+	qUtils.send_message(cPickle.dumps(test),qUtils.PROX_PORT) #after change for web_list maybe we will need deepcopy (not sure)
